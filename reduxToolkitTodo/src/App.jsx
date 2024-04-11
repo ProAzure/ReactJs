f4c540fa03236af251c191497e0bf74206176699
Atharva3749 <atharvashinde64@gmail.com>
2024-04-11 21:15:22 +0530

    more project added

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import Todos from './components/Todos'
+import AddTodo  from './components/addTodo'
+import './App.css'
+
+
+function App() {
+  return (
+  <>
+  <h1>Learn about redux toolkit</h1>
+  <AddTodo/>
+  <Todos/>
+  </>
+  )
+}
+
+export default App
